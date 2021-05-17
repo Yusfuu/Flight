95f3bbdf9763d986511331b61e83188ec00c38ba
Yusfuu <youssefhajjari01@gmail.com>
2021-05-17 20:08:12 +0100

    rename header

--- views/_404.php
+++ views/_404.php
@@ -14,7 +14,7 @@
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
         <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-          <span class="fs-4">Simple header</span>
+          <span class="fs-4">FlyBoy</span>
         </a>
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
